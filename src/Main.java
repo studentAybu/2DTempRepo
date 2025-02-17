c821f959e8202364cb3e1c614916658c6547a46d
studentAybu <23050161004@aybu.edu.tr>
2025-02-17 15:07:14 +0300

    Add Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,7 @@ import entities.Shape2D;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 
 
 public class Main {
